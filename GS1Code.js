98b08a88308e1b5b9aca5c141855207494b5b241
aneggi <ing.alessandro@yahoo.it>
2018-08-24 10:30:05 +0200

    Actualy GS1 Prefix and Serial Number can be variable, improve Unittest

--- GS1Code.js
+++ GS1Code.js
@@ -1,42 +1,65 @@
-// 00 080179380010044425
 var sscc_test = '08017938001004442'; //17 cifre
-//var GS1CompanyPrefix = '';
-//const applicationIdentifier = '00';
-//const extensionDigit = '0';
-//var serialReference = '';
-//var checkDigit = '';
 
-
-//console.log('SSCC_TEST=' + sscc_test);
-//console.log('Test method -> Create base SSCC:' + createBase() );
-
-//const sscc1 = sscc;
-
-
-//console.log('Test method -> ssccLong17: (17)' + sscc().ssccLong17('08017938001004442') );
-//console.log('Test method -> ssccLong17: (18)' + sscc().ssccLong17('080179380010044421') );
-//console.log('Test method -> ssccLong17: (20)' + sscc().ssccLong17('00080179380010044422') );
-//console.log('Test method -> ssccLong17: (20)*' + sscc().ssccLong17('01080179380010044422') );
-//console.log('Test method -> Next:' + sscc().ssccLong17('08017938001004442') );
-
- let objSscc = { 
-        extensionDigit : '0' ,
-        GS1CompanyPrefix: '',
-        GS1Serial: '000', 
-        serialNumber: '000000',
-        checkDigit: ''
+let objSscc = {   // total length fixed to 18 (last one is check digit)
+        extensionDigit : '0' , // fixed 1 
+        GS1CompanyPrefix: '', // variable from 7 to 9
+        serialNumber: '000000',   // variable 7 to 9
+        checkDigit: '' // fixed 1
         }
 
-function  sscc () {
+const applength = 2;
+const extensiondigitlength = 1;
+const sscclength = 18;
+let gs1prefixlength = -1;
+let serialnumberlength = -1;
+    
 
+function setLengths(_gs1prefix)  {
+    if(_gs1prefix.length < 6 || _gs1prefix.length > 9) return undefined
+    gs1prefixlength= _gs1prefix.length;
+    serialnumberlength= 18-2- _gs1prefix.length;
+}
+
+function  sscc () {
+    this.split = function (_sscc) {
+            if(_sscc == undefined) return undefined
+            return { 
+                    extensionDigit : _sscc.substring(0, extensiondigitlength) ,
+                    GS1CompanyPrefix: _sscc.substring(extensiondigitlength,extensiondigitlength+gs1prefixlength),
+                    serialNumber: parseInt( _sscc.substring(extensiondigitlength+gs1prefixlength, sscclength-1)),
+                    checkDigit: _sscc.substring(sscclength-1,sscclength )
+                }
+    }
+    this.next = function (_sscc, _gs1prefix) {
+        setLengths(_gs1prefix);
+        _sscc = this.ssccUniform(_sscc);
+        _sscc = this.split(_sscc);
+        if (!_sscc) return undefined;
+        else {
+            let i =0;
+            let serialend = '';
+            for(i=0 ; i< serialnumberlength;i++) {
+                serialend.concat('9');
+            }
+            var _sn = parseInt( _sscc.serialNumber);
+            _sn = (_sn + 1).toString();
+            if(_sn.length> serialnumberlength ) {_sn = _sn.substring(1,serialnumberlength+1);}
+            _sscc.serialNumber =  _sn;          
+            var sscc2 = '' + _sscc.extensionDigit +  _sscc.GS1CompanyPrefix + _sscc.serialNumber
+            const ssccCheck = this.digitCalculate(sscc2);      
+            sscc2 = '' + String(sscc2) + String(ssccCheck);
+            return  sscc2
+        }
+    }
         
-    this.ssccLong17 = function(_sscc){
+    this.ssccUniform = function(_sscc){ //check long and give a 17 digits
         var ssccText = '';
         ssccText = _sscc;
         //if(ssccText.length >20 || ssccText.length< 17 ) 
-        if(ssccText.length === 17) return _sscc
-        if(ssccText.length === 18) return _sscc.substring(0,17)
-        if(_sscc.substring(0,2) === '00' && ssccText.length === 20) _sscc.substring(2,20)
+        if(ssccText.length === 17) return ssccText
+        if(ssccText.length === 18) return ssccText.substring(0,17)
+        const _sscc2 = ssccText.substring(0,2);
+        if(_sscc2 == '00' && ssccText.length == 20) return ssccText.substring(2,19)  
         return undefined
     }
 
@@ -51,7 +74,6 @@ function  sscc () {
             //console.log (`Calculating...is ${i % 2 == 0}  (Stringa[${i}]- ${_sscc[i]}) -> ${_value} Sum:${_calculateDigit}  `);
         }
         const lastDigitCalculated =_calculateDigit % 10 ;
-        //console.log('Units: ', lastDigitCalculated);
         if (lastDigitCalculated >4 ) checkDigit = 10 -  lastDigitCalculated;
         else checkDigit = lastDigitCalculated;
         //console.log(this.split(checkDigit));
@@ -63,26 +85,10 @@ function  sscc () {
         else _sscc = _sscc + digitCalculate(_sscc);
         return _sscc
     }
-    this.split = function (_sscc) {
-        if (!this.check(_sscc)) return { result: 'error', message: 'Check digit is not right...' }
-        
-        else {
-            return { 
-                result: 'OK',
-                sscc: [{
-                    extensionDigit : _sscc.substring(0, 1) ,
-                GS1CompanyPrefix: _sscc.substring(1, 8),
-                GS1Serial: _sscc.substring(8, 11), 
-                serialNumber: _sscc.substring(11, 17),
-                checkDigit: _sscc.substring(17, 18)
-                }]
-            }
-        }
-    }
-
+    
     this.check = function(_sscc) {
-        const _checkResult = this.digitCalculate(_sscc.substring(0,17));
-        console.log(_checkResult + ' - Substring:' +_sscc.substring(17,18));
+        const _checkResult = this.digitCalculate(_sscc);
+        //console.log(_checkResult + ' - Substring:' +_sscc.substring(17,18));
         if (_checkResult == _sscc.substring(17,18)) return true
         else return false
     }
@@ -90,5 +96,7 @@ function  sscc () {
 }
 
 module.exports = new sscc();
-const mySscc = new sscc();
-console.log('Test method -> ssccLong17: (16)' + mySscc.check( '080179380010044425') );
+const mySscc = new sscc('1234567');
+
+console.log('Final result:' + mySscc.next( '012349999999999992','123456') );
+//console.log('Test split:' + mySscc.split( '080179380010044425').sscc.extensionDigit );
--- test/GS1Code.test.js
+++ test/GS1Code.test.js
@@ -10,6 +10,31 @@ describe('SSCC digitCalculate', () =>{
 
 });
 
+describe('SSCC next', () =>{
+
+    it('should return undefined', ()=> {
+        const result = sscc.next('0801793800100444251','123413');
+        expect(result).toBe(undefined);
+    });
+
+    it('should return code: 080179380010044432', ()=> {
+        const result = sscc.next('080179380010044425','123413');
+        expect(result).toBe('080179380010044432');
+    });
+
+    it('should return code: 012349900000000002', ()=> {
+        const result = sscc.next('012349999999999992','123493');
+        expect(result).toBe('012349900000000002');
+    });
+
+    it('should return undefined', ()=> {
+        const result = sscc.next('0801793800100444','123413');
+        expect(result).toBe(undefined);
+    });
+
+
+});
+
 describe('SSCC check', () =>{
 
     it('should return true', ()=> {
@@ -21,6 +46,50 @@ describe('SSCC check', () =>{
         const result = sscc.check('080189380010044425');
         expect(result).toBe(false);
     });
+    
+
+});
+
+
+describe('SSCC first', () =>{
+
+    it('should return true', ()=> {
+        const result = sscc.check('080179380010044425');
+        expect(result).toBe(true);
+    });
+
+    it('should return false', ()=> {
+        const result = sscc.check('080189380010044425');
+        expect(result).toBe(false);
+    });
+    
+
+});
+
+describe('SSCC uniform', () =>{
+
+    it('should return 08017938001004442', ()=> {
+        const result = sscc.ssccUniform('08017938001004442');
+        expect(result).toBe('08017938001004442');
+    });
+
+    it('should return 08017938001004442', ()=> {
+        const result = sscc.ssccUniform('080179380010044425');
+        expect(result).toBe('08017938001004442');
+    });
+
+    it('should return 08017938001004442', ()=> {
+        const result = sscc.ssccUniform('00080179380010044425');
+        expect(result).toBe('08017938001004442');
+    });
+
+    it('should return false', ()=> {
+        const result = sscc.ssccUniform('01080179380010044425');
+        expect(result).toBe(undefined);
+    });
+
+    
+    
     
 
 });
